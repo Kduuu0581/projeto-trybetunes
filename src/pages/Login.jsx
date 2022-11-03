33c21c5ddf14592acb032d056c72412e9c180400
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-11-03 16:20:31 -0300

    2 - Crie um formulário para identificação

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../Loading';
 
 class Login extends Component {
+  state = {
+    nome: '',
+    loading: false,
+    click: false,
+  };
+
+  handleClick = () => {
+    this.setState({ click: true });
+    const { nome } = this.state;
+    this.setState({ loading: true }, async () => {
+      await createUser({ name: nome });
+      this.setState({ loading: false });
+    });
+  };
+
   render() {
+    const { nome, loading, click } = this.state;
+    const caracterMin = 3;
+
     return (
       <div data-testid="page-login">
-        Login
+        <h1>Login</h1>
+        <input
+          type="text"
+          data-testid="login-name-input"
+          onChange={ ({ target: { value } }) => this.setState({ nome: value }) }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ nome.length < caracterMin }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+        {loading === true && (<Loading />)}
+        {(!loading && click) && (<Redirect to="/search" />)}
       </div>
     );
   }
