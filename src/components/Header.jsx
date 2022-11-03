8b768741af9368aeb4f4fc078ecd7a6034f71176
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-11-03 17:50:18 -0300

    4 - Crie os links de navegação no cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -19,6 +20,9 @@ class Header extends Component {
         <h1>TybeTunes</h1>
         { !user && <Loading /> }
         { user && (<p data-testid="header-user-name">{user.name}</p>)}
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
