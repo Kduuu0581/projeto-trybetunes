5c292bd75e4af5dea87dc69b29f67a04c3f4c3f5
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-11-03 18:26:12 -0300

    5 - Crie o formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    artist: '',
+  };
+
+  handleClick = () => {
+    this.state({ artist: '' });
+  };
+
   render() {
+    const { artist } = this.state;
+    const caracterMin = 2;
     return (
-      <div data-testid="page-search">
-        Search
-      </div>
+      <>
+        <div data-testid="page-search">
+          Search
+        </div>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ artist }
+          onChange={ ({ target: { value } }) => {
+            this.setState({ artist: value });
+          } }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ artist.length < caracterMin }
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
+      </>
     );
   }
 }
