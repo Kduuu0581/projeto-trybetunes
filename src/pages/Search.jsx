1af9955654ac83499a23d9adf1c8262a2fc691a8
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2022-11-04 17:40:25 -0300

    6 - Faça a requisição para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,39 +1,73 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends Component {
   state = {
     artist: '',
+    name: '',
+    results: [],
+    loading: false,
   };
 
-  handleClick = () => {
-    this.state({ artist: '' });
+  handleClick = async () => {
+    const { artist } = this.state;
+    this.setState({ artist: '', loading: true });
+    const resultados = await searchAlbumsAPI(artist);
+    this.setState({ results: resultados, loading: false });
   };
 
   render() {
-    const { artist } = this.state;
+    const { artist, results, name, loading } = this.state;
     const caracterMin = 2;
     return (
-      <>
-        <div data-testid="page-search">
-          Search
+      <div data-testid="page-search">
+        Search
+        {!loading && (
+          <>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              value={ artist }
+              onChange={ ({ target: { value } }) => {
+                this.setState({ artist: value, name: value });
+              } }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ artist.length < caracterMin }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+          </>
+        )}
+        {loading && <Loading />}
+        <div>
+          {name.length >= 2 && results.length > 0 ? (
+            <div>
+              <p>{`Resultado de álbuns de: ${name}`}</p>
+              {results.map((album, index) => (
+                <div key={ `${album.artistId} ${index}` }>
+                  <img src={ album.artworkUrl100 } alt={ album.artistName } />
+                  <h2>{`Álbum ${album.trackCount} ${album.collectionName}`}</h2>
+                  <p>{`Artista ${album.artistName}`}</p>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    Álbum
+                  </Link>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <h2>Nenhum álbum foi encontrado</h2>
+          )}
         </div>
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ artist }
-          onChange={ ({ target: { value } }) => {
-            this.setState({ artist: value });
-          } }
-        />
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          disabled={ artist.length < caracterMin }
-          onClick={ this.handleClick }
-        >
-          Pesquisar
-        </button>
-      </>
+      </div>
     );
   }
 }
